d745fdb39fdaa8372406322b1bc886245e609c85
Karan <ikarn.dev@gmail.com>
2025-02-22 19:14:38 +0530

    Commit

---
+++ Basic/Basic_Syntax.java
@@ -0,0 +1,24 @@
+/*1.  Java Program to Print an Integer (Entered by the User) \[E]
+2.  Java Program to Add Two Integers \[E]
+3.  Java Program to Multiply two Floating Point Numbers \[E]
+4.  Java Program to Find ASCII Value of a character \[E]
+5.  Java Program to Compute Quotient and Remainder \[E]
+6.  Java Program to Swap Two Numbers \[E]
+7.  Java Program to Check Whether a Number is Even or Odd \[E]
+8.  Java Program to Check Whether an Alphabet is Vowel or Consonant \[E]
+9.  Java Program to Check Whether a Character is Alphabet or Not \[E]
+10. Java Program to Display Alphabets (A to Z) using loop \[E]
+11. Java Program to Iterate over enum \[E]
+12. Java Program to Determine the name and version of the operating system \[E]
+132.Java Program to Check if two of three boolean variables are true \[E] */
+
+//Answers
+//1.  Java Program to Print an Integer (Entered by the User) \[E]
+
+import java.util.Scanner;
+class Basic_Syntax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
